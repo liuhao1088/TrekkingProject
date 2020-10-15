6037b63ddccd43604f9d0ad7a8775e9f5644c554
liuhao1088 <1350569467@qq.com>
2020-10-15 19:14:54 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,34 @@ Page({
    */
   data: {
     imgUrl:[
-      'https://img14.360buyimg.com/ddimg/jfs/t1/145648/22/10851/498741/5f881e74E2709c116/8970ce38e6976c24.png',
-      'https://img13.360buyimg.com/ddimg/jfs/t1/124096/22/15001/541952/5f880f11Ee66cfa7c/9325e622eb5fc1ae.png',
-      'https://img10.360buyimg.com/ddimg/jfs/t1/147161/30/10873/458621/5f880f21Ee41da51c/aa04d76e0d9a648b.png',
-      'https://img14.360buyimg.com/ddimg/jfs/t1/120330/11/15225/476708/5f880f42E71bd8a98/1b49271871ae29e0.png'
+      {
+        url:'https://img14.360buyimg.com/ddimg/jfs/t1/145648/22/10851/498741/5f881e74E2709c116/8970ce38e6976c24.png',
+        width:"95%",
+        height:"95%",
+        marginTop:"20rpx"
+      },
+      {
+        url:'https://img13.360buyimg.com/ddimg/jfs/t1/124096/22/15001/541952/5f880f11Ee66cfa7c/9325e622eb5fc1ae.png',
+        width:"95%",
+        height:"95%",
+        marginTop:"20rpx"
+      },
+      {
+        url:'https://img10.360buyimg.com/ddimg/jfs/t1/147161/30/10873/458621/5f880f21Ee41da51c/aa04d76e0d9a648b.png',
+        width:"95%",
+        height:"95%",
+        marginTop:"20rpx"
+      },
+      {
+        url:'https://img14.360buyimg.com/ddimg/jfs/t1/120330/11/15225/476708/5f880f42E71bd8a98/1b49271871ae29e0.png',
+        width:"95%",
+        height:"95%",
+        marginTop:"20rpx"
+      }
+      
+      
+      
+      
     ],
     links: [
       '../test/test',
@@ -21,13 +45,78 @@ Page({
      indicatorDots: true,
      autoplay: false,
      vertical: true,
-     circular: true
+     circular: true,
   },
 
   swiperChange: function (e) {
+    console.log(e.detail.current);
+    var index = e.detail.current;
     this.setData({
       active: e.detail.current
+      
     })
+    if(index == 0){
+      this.setData({
+        'imgUrl[0].marginTop': "0rpx",
+        'imgUrl[0].width':"100%",
+        'imgUrl[0].height':"100%",
+        'imgUrl[1].marginTop': "20rpx",
+        'imgUrl[1].width':"95%",
+        'imgUrl[1].height':"95%",
+        'imgUrl[2].marginTop': "20rpx",
+        'imgUrl[2].width':"95%",
+        'imgUrl[2].height':"95%",
+        'imgUrl[3].marginTop': "20rpx",
+        'imgUrl[3].width':"95%",
+        'imgUrl[3].height':"95%",
+      })
+    }else if(index == 1){
+      this.setData({
+        'imgUrl[1].marginTop': "0rpx",
+        'imgUrl[1].width':"100%",
+        'imgUrl[1].height':"100%",
+        'imgUrl[0].marginTop': "20rpx",
+        'imgUrl[0].width':"95%",
+        'imgUrl[0].height':"95%",
+        'imgUrl[2].marginTop': "20rpx",
+        'imgUrl[2].width':"95%",
+        'imgUrl[2].height':"95%",
+        'imgUrl[3].marginTop': "20rpx",
+        'imgUrl[3].width':"95%",
+        'imgUrl[3].height':"95%",
+      })
+    }else if(index == 2){
+      this.setData({
+        'imgUrl[2].marginTop': "0rpx",
+        'imgUrl[2].width':"100%",
+        'imgUrl[2].height':"100%",
+        'imgUrl[1].marginTop': "20rpx",
+        'imgUrl[1].width':"95%",
+        'imgUrl[1].height':"95%",
+        'imgUrl[3].marginTop': "20rpx",
+        'imgUrl[3].width':"95%",
+        'imgUrl[3].height':"95%",
+        'imgUrl[0].marginTop': "20rpx",
+        'imgUrl[0].width':"95%",
+        'imgUrl[0].height':"95%",
+      })
+    }else if(index == 3){
+      this.setData({
+        'imgUrl[3].marginTop': "0rpx",
+        'imgUrl[3].width':"100%",
+        'imgUrl[3].height':"100%",
+        'imgUrl[0].marginTop': "20rpx",
+        'imgUrl[0].width':"95%",
+        'imgUrl[0].height':"95%",
+        'imgUrl[1].marginTop': "20rpx",
+        'imgUrl[1].width':"95%",
+        'imgUrl[1].height':"95%",
+        'imgUrl[2].marginTop': "20rpx",
+        'imgUrl[2].width':"95%",
+        'imgUrl[2].height':"95%",
+      })
+    }
+    
   },
   swipclick: function (e) {
     console.log(this.data.links[this.data.active]);
@@ -73,7 +162,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that = this;
+    var marginTop =  that.data.imgUrl[0].marginTop;
+    var width =  that.data.imgUrl[0].width;
+    var height =  this.data.imgUrl[0].height;
+    this.setData({
+      'imgUrl[0].marginTop': "0rpx",
+      'imgUrl[0].width':"100%",
+      'imgUrl[0].height':"100%"
+    })
+    console.log(marginTop);
+    console.log(width);
+    console.log(height);
+
   },
 
   /**
