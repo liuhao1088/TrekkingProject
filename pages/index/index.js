40dba3d896690e0509fb9bfc28063eb779163ebc
liuhao1088 <1350569467@qq.com>
2020-10-21 10:27:29 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -41,7 +41,7 @@ Page({
       '',
       ''
     ],
-     active: 0,
+    swiperIndex: 0,
      indicatorDots: true,
      autoplay: false,
      vertical: true,
@@ -50,80 +50,16 @@ Page({
 
   swiperChange: function (e) {
     // console.log(e.detail.current);
-    var index = e.detail.current;
-    var that = this;
     this.setData({
-      active: e.detail.current
+      swiperIndex: e.detail.current
       
     })
-    if(index == 0){
-      this.setData({
-        'imgUrl[0].marginTop': "0rpx",
-        'imgUrl[0].width':"100%",
-        'imgUrl[0].height':"100%",
-        'imgUrl[1].marginTop': "20rpx",
-        'imgUrl[1].width':"95%",
-        'imgUrl[1].height':"95%",
-        'imgUrl[2].marginTop': "20rpx",
-        'imgUrl[2].width':"95%",
-        'imgUrl[2].height':"95%",
-        'imgUrl[3].marginTop': "20rpx",
-        'imgUrl[3].width':"95%",
-        'imgUrl[3].height':"95%",
-      })
-    }else if(index == 1){
-        that.setData({
-          'imgUrl[0].marginTop': "20rpx",
-          'imgUrl[0].width':"95%",
-          'imgUrl[0].height':"95%",
-          'imgUrl[2].marginTop': "20rpx",
-          'imgUrl[2].width':"95%",
-          'imgUrl[2].height':"95%",
-          'imgUrl[3].marginTop': "20rpx",
-          'imgUrl[3].width':"95%",
-          'imgUrl[3].height':"95%",
-          'imgUrl[1].marginTop': "0rpx",
-          'imgUrl[1].width':"100%",
-          'imgUrl[1].height':"100%",     
-        })
-      
-    }else if(index == 2){
-      this.setData({
-        'imgUrl[2].marginTop': "0rpx",
-        'imgUrl[2].width':"100%",
-        'imgUrl[2].height':"100%",
-        'imgUrl[1].marginTop': "20rpx",
-        'imgUrl[1].width':"95%",
-        'imgUrl[1].height':"95%",
-        'imgUrl[3].marginTop': "20rpx",
-        'imgUrl[3].width':"95%",
-        'imgUrl[3].height':"95%",
-        'imgUrl[0].marginTop': "20rpx",
-        'imgUrl[0].width':"95%",
-        'imgUrl[0].height':"95%",
-      })
-    }else if(index == 3){
-      this.setData({
-        'imgUrl[3].marginTop': "0rpx",
-        'imgUrl[3].width':"100%",
-        'imgUrl[3].height':"100%",
-        'imgUrl[0].marginTop': "20rpx",
-        'imgUrl[0].width':"95%",
-        'imgUrl[0].height':"95%",
-        'imgUrl[1].marginTop': "20rpx",
-        'imgUrl[1].width':"95%",
-        'imgUrl[1].height':"95%",
-        'imgUrl[2].marginTop': "20rpx",
-        'imgUrl[2].width':"95%",
-        'imgUrl[2].height':"95%",
-      })
-    }
     
   },
   swipclick: function (e) {
-    console.log(this.data.links[this.data.active]);
+    console.log(this.data.links[this.data.swiperIndex]);
     wx.navigateTo({
-      url: this.data.links[this.data.active]
+      url: this.data.links[this.data.swiperIndex]
     })
   },
   hdRecorderSeriesdj:function(e){
@@ -146,36 +82,11 @@ Page({
       url: "../truckReconrderSeries/truckReconrderSeries"
     })
   },
-
-  previewImg:function(e){
-    console.log(e.currentTarget.dataset.index);
-    var index = e.currentTarget.dataset.index;
-   
-    console.log(this.data.imgArr);
-    wx.previewImage({
-      current: this.data.imgArr[index],     //当前图片地址
-      urls: this.data.imgArr,               //所有要预览的图片的地址集合 数组形式
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    var marginTop =  that.data.imgUrl[0].marginTop;
-    var width =  that.data.imgUrl[0].width;
-    var height =  this.data.imgUrl[0].height;
-    this.setData({
-      'imgUrl[0].marginTop': "0rpx",
-      'imgUrl[0].width':"100%",
-      'imgUrl[0].height':"100%"
-    })
-    console.log(marginTop);
-    console.log(width);
-    console.log(height);
+
 
   },
 
