12b229641a6fd42f823f91aa5b6850a86745f19a
liuhao1088 <1350569467@qq.com>
2020-10-26 11:58:47 +0800

    更新

--- pages/video/video.js
+++ pages/video/video.js
@@ -38,9 +38,31 @@ Page({
         title:'USB系列产品',
         yw:'K6'
       }
-    ]
+    ],
+    indexCurrent:null,
   },
-
+  video_play(e) {
+    var curIdx = e.currentTarget.id;
+    // 没有播放时播放视频
+    console.log(curIdx)
+    if (!this.data.indexCurrent) {
+      this.setData({
+        indexCurrent: curIdx
+      })
+      var videoContext = wx.createVideoContext(curIdx,this) //这里对应的视频id
+      videoContext.play()
+    } else { // 有播放时先将prev暂停，再播放当前点击的current
+      var videoContextPrev = wx.createVideoContext(this.data.indexCurrent,this)//this是在自定义组件下，当前组件实例的this，以操作组件内 video 组件（在自定义组件中药加上this，如果是普通页面即不需要加）
+      if (this.data.indexCurrent != curIdx) {
+        videoContextPrev.pause()
+        this.setData({
+          indexCurrent: curIdx
+        })
+        var videoContextCurrent = wx.createVideoContext(curIdx,this)
+        videoContextCurrent.play()
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
